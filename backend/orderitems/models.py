1724d1d0554a49a94e39919134fde8637836a8c7
Mark Sherrington <git@marksherrington.uk>
2020-02-15 14:01:49 +0000

    small updates to models

--- backend/orderitems/models.py
+++ backend/orderitems/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class OrderItem(models.Model):
     quantity = models.PositiveIntegerField()
-    item = models.ForeignKey(null=True, blank=True, on_delete=models.CASCADE, to='items.Item')
+    item = models.ForeignKey(null=True, blank=True, on_delete=models.DO_NOTHING, to='items.Item')
 
     def __str__(self):
         return '{} x {}'.format(self.quantity, self.item.name)
--- backend/orders/models.py
+++ backend/orders/models.py
@@ -5,7 +5,7 @@ from orders.services.order_number_service import generate_order_number
 
 class Order(models.Model):
     order_number = models.PositiveIntegerField(unique=True, default=generate_order_number)
-    order_items = models.ForeignKey(null=True, blank=True, on_delete=models.CASCADE, to='orderitems.OrderItem')
+    order_items = models.ManyToManyField(blank=True, to='orderitems.OrderItem', related_name='order')
     submitted_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
