7c11931e93ee6898372c87c2c8a376321d4eb1c9
Mark Sherrington <git@marksherrington.uk>
2020-02-15 22:28:42 +0000

    clean up urls

--- backend/basket/urls.py
+++ backend/basket/urls.py
@@ -1,12 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 
-from items import views as item_views
 from orders import views as order_views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/submit-order', order_views.OrderViewSet.as_view({'post': 'list'})),
-    path('api/items/', item_views.ItemList.as_view()),
 ]
