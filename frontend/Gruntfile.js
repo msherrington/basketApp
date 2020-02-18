7a569369b93a14581d4eee41b5ce5cb68eb19800
Mark Sherrington <git@marksherrington.uk>
2020-02-18 21:08:55 +0000

    simplify grunt file

--- frontend/Gruntfile.js
+++ frontend/Gruntfile.js
@@ -1,22 +1,10 @@
 module.exports = function(grunt) {
 
     grunt.initConfig({
-        jshint: {
-            files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
-        },
-        watch: {
-            files: ['<%= jshint.files %>'],
-            tasks: ['jshint']
-        }
+
     });
-
-
-
+    
     grunt.loadNpmTasks('grunt-serve');
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-
-    grunt.registerTask('default', ['jshint']);
 
 };
 
