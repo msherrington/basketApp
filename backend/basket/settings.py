b215ac3d70044ce81f9196f701a14e61c443872a
Mark Sherrington <git@marksherrington.uk>
2020-02-18 22:04:04 +0000

    debug

--- backend/basket/settings.py
+++ backend/basket/settings.py
@@ -15,7 +15,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'abcde'  # KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False  # DEBUGGING or False
+DEBUG = True # False  # DEBUGGING or False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
